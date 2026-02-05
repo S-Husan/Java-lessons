22157de209e5132c442cd8b5d0dfc0dc36cca7c0
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-02-05 18:59:35 +0500

    first lessons

---
+++ Demo.java
@@ -0,0 +1,75 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("SIUT");
+//     }
+// /**  This is something
+//     @param p is something
+//     @return the return value 
+//  */
+// // single line comment
+// // multi line comment 
+// // documintation comment
+// }
+// //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// package siut.soc;
+// /**This is a class level documentation */
+
+// import java.time.chrono.JapaneseEra;
+
+// public class MyDocClass {
+//     /**This is a public field */
+//     public int age;
+// }
+
+// /** This is a protected field */
+// protected float f;
+
+// // No documentation comment for this 
+// private char c;
+
+// /** This is the getSum method and it adds two numbers 
+//      @param it is the fist number
+//      @param it is the second number
+//      @param it You get the sum of two number 
+//  */
+
+// public int getSum (int i, int j){
+//     return i + j;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Demo 
+// {
+// public static void main(String[] list_name)
+// {
+//     System.out.print("hello world");}
+//     // Decinal literal
+//     short s = 32;
+//     int i = 32;
+// }
+
+ 
+
+
+// public class Demo
+// {
+//     public static void main(String[] args)
+//     {
+//         boolean b = 5 > 10;
+//         System.out.println(b);
+//     }
+// }
